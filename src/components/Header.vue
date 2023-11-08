2c1d8379498b50d3210f00b6dcca32fe7a3b3296
Ilídio Martins <ilidio.martins@gmail.com>
2023-11-08 20:32:53 +0000

    Foto icon Ilidio added

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,7 @@
           aria-expanded="false"
           aria-label="Toggle navigation"
       >
-        <span class="fas fa-bars"></span>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
       </button>
       <div class="collapse navbar-collapse" id="navbarExample01">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -33,8 +33,7 @@
       </div>
 
       <!-- A minha foto -->
-      <img
-          src="https://lh3.googleusercontent.com/-V7jCih42Izg/AAAAAAAAAAI/AAAAAAAAAAA/AML38-tcalWQmGK1jRtzSsd1YVgjquWUZA/photo.jpg?sz=46"
+      <img src="../assets/images/photo.jpg"
           alt="Ilídio Martins" class="img-fluid rounded-circle photo">
     </div>
   </nav>
